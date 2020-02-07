b05cd69116d6f6e6c891da85cea6b1ce138b0fb2
lightwidow <mario@codevana.co>
2020-02-07 06:57:37 -0500

    Updates

---
+++ precache-manifest.467abd43bd54530653acb2a8a666bd41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2deebe45e7b4e4167a5695793650542c",
+    "url": "/snake-game-demo/index.html"
+  },
+  {
+    "revision": "e27555f44ff0aa5ea3d8",
+    "url": "/snake-game-demo/static/css/main.21836296.chunk.css"
+  },
+  {
+    "revision": "71617a7137192c8d4297",
+    "url": "/snake-game-demo/static/js/2.813a9d1e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/snake-game-demo/static/js/2.813a9d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27555f44ff0aa5ea3d8",
+    "url": "/snake-game-demo/static/js/main.cc3dd245.chunk.js"
+  },
+  {
+    "revision": "50c4076f60d4e8fa4238",
+    "url": "/snake-game-demo/static/js/runtime-main.3e65d375.js"
+  }
+]);
